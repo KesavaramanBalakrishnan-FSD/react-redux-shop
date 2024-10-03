138a8bc6eca2622f052b337f9f0b49fd83aaede8
KesavaramanBalakrishnan-FSD <kesavaramanbalakrishnan@gmail.com>
2024-10-03 21:11:03 +0530

    Initial commit with project files

---
+++ src/containers/Header.js
@@ -0,0 +1,14 @@
+import React from "react";
+import "../App.css";
+
+const Header = () => {
+  return (
+    <div className="header ui fixed menu">
+      <div className="ui container center">
+        <h2>ReduxShop</h2>
+      </div>
+    </div>
+  );
+};
+
+export default Header;
